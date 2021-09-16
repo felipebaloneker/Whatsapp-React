87552412b6f4c259aaa41e0bbbc3de8bb9c0cd2c
felipebaloneker <felipebaloneker23@gmail.com>
2021-09-16 19:07:05 -0300

    adicionado chatlistitem

--- src/components/ChatListItem/index.jsx
+++ src/components/ChatListItem/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Avatar from "../../assets/images/avatar.svg";
+
 import "./style.css";
 
 export default () =>{
     return(
         <>
-            <div>...</div>
+            <div className='chatListItem'>
+                <img className='chatListItem-avatar' src={Avatar} alt=''></img>
+                <div className='chatListItem-lines'>
+                    <div className='chatListItem-line'>
+                        <div className='chatListItem-name'>Felipe Baloneker</div>
+                        <div className='chatListItem-date'>19:00</div>
+                    </div>
+                    <div className='chatListItem-line'>
+                        <div className='chatListItem-lastMsg'>
+                            <p>Opa, tudo bem</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }

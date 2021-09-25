c56252bd037698b190d0cb77c915cc685bf3b5c7
felipebaloneker <felipebaloneker23@gmail.com>
2021-09-25 20:14:45 -0300

    adicionando background para menssagem do usuario

--- src/components/MessageItem/index.jsx
+++ src/components/MessageItem/index.jsx
@@ -5,12 +5,17 @@ export function MessageItem({data,user}){
     return(
         <>
             <div 
-            className='messageLine'
-            style={{
-                justifyContent:user.id == data.author ? 'flex-end': "flex-start"
-            }}
+                className='messageLine'
+                style={{
+                  justifyContent:user.id == data.author ? 'flex-end': "flex-start"
+                }}
             > 
-                <div className='messageItem'>
+                <div 
+                    className='messageItem'
+                    style={{
+                        backgroundColor:user.id == data.author ? '#056162' :'#262D31'
+                    }}
+                >
                     <div className='messageText'>{data.body}</div>
                     <div className='messageDate'>19:00</div>
                 </div>

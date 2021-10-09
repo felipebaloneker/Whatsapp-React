80c8438ff76da143d05ecb0a5507c0f88728e518
Felipe Baloneker <felipebaloneker23@gmail.com>
2021-10-08 23:16:03 -0300

    tela de login finalizada

--- src/components/TelaLogin/index.jsx
+++ src/components/TelaLogin/index.jsx
@@ -1,7 +1,6 @@
 import './style.css'
 import Api from '../../services/Api'
 
-
 export default({onReceive}) =>{
     const handleFacebookLogin = async () =>{
         let result = await Api.fbPopup();
@@ -14,10 +13,25 @@ export default({onReceive}) =>{
     }
 
     return(
-        <div>
-            <button
-                onClick={handleFacebookLogin}
-            >Login com Facebook</button>
+        <div className='container'>
+            <div className='container-header'>
+            </div>
+            <div className="containe-main">
+                <div className='container-center'>
+                    <div>
+                        <span>WhatsApp Web</span>
+                        <p>
+                            Para acessar, Utilizer sua conta do Facebook
+                        </p>
+                    </div>
+                    <div className='wrp-facebook'>
+                        <input type='button' value='Login com Facebook'
+                        className="btn-login"
+                        onClick={handleFacebookLogin}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
